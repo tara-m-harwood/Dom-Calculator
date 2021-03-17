45db1a2fb49e13d4fafd19887d4294fcf135afe8
Tara Harwood <tara.m.harwood@gmail.com>
2021-03-17 16:39:55 -0700

    basically working

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
-console.log("dom calc")
+console.log('dom calc')
 
+const $screen = document.getElementById('screen')
 const $spans = document.querySelectorAll('span')
 
 $spans.forEach((span) => {
-    span.addEventListener("click", doSomething)
+    span.addEventListener('click', processButtonPress)
     })
 
-function doSomething() {
-    console.log(this.textContent)
+function processButtonPress() {
+    let buttonValue = this.textContent
+    let screenValue = $screen.textContent
+    if (buttonValue ==='C') {
+        $screen.textContent = ''
+    }
+    else if (screenValue === "ERROR") {
+        return
+    } else if (buttonValue === "=") {
+        performCalc()
+    } else {
+        addToDisplay(buttonValue)
+    }}
+
+
+function addToDisplay(buttonValue) {
+    console.log(buttonValue)
+    $screen.textContent += buttonValue
 }
 
-
+function performCalc() {
+    userInput = $screen.innerText
+    userInput = userInput.replace("x", "*")
+    userInput = userInput.replace("÷", "/")
+    console.log(userInput)
+    try {
+        (new Function('return '+userInput)())
+    } catch (e) {
+        console.error(e instanceof SyntaxError);
+        $screen.innerText = "ERROR"
+    }
+    const calc = (new Function('return '+userInput)())
+    $screen.innerText = calc
+}    
 
